d49c3fce7e3a12cd0ce6c3ce832e1f6dc63f9481
liqiang <1936580@qq.com>
2017-11-29 23:54:57 +0800

    add zhuanye area filed

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -5,7 +5,7 @@ from .models import Benke, Shuoshi
 class BenkeAdmin(admin.ModelAdmin):
     list_display = ('id', 'zhongwen', 'jigou', 'gpa', 'tofel', 'ielts', 'sat', 'act')
     fieldsets = [
-        ('学校信息', {'fields':['liuxue_leixing', 'offer_huoqu', 'tiaojian', 'guojia', 'zhongwen', 'xueli', 'xuezhi', 'jiangxuejin', 'zhuanye']}),
+        ('学校信息', {'fields':['liuxue_leixing', 'offer_huoqu', 'tiaojian', 'guojia', 'zhongwen', 'xueli', 'xuezhi', 'jiangxuejin', 'zhuanye', 'area']}),
         ('代理信息', {'fields':['jigou', 'jigou_city', 'url']}),
         ('学员信息', {'fields':['chengshi', 'xuexiao', 'xuexiaoleibie', 'xueli_leibie', 'xueli_chengdu', 'gaokao',
                             'gpa', 'ielts', 'tofel', 'sat', 'act']}),
@@ -18,7 +18,7 @@ admin.site.register(Benke, BenkeAdmin)
 class ShuoshiAdmin(admin.ModelAdmin):
     list_display = ('id', 'zhongwen', 'jigou', 'gpa', 'tofel', 'ielts', 'gre', 'gmat')
     fieldsets = [
-        ('学校信息', {'fields':['liuxue_leixing', 'offer_huoqu', 'tiaojian', 'guojia', 'zhongwen', 'xueli', 'xuezhi', 'jiangxuejin', 'zhuanye']}),
+        ('学校信息', {'fields':['liuxue_leixing', 'offer_huoqu', 'tiaojian', 'guojia', 'zhongwen', 'xueli', 'xuezhi', 'jiangxuejin', 'zhuanye', 'area']}),
         ('代理信息', {'fields':['jigou', 'jigou_city', 'url']}),
         ('学员信息', {'fields':['chengshi', 'zhuanye_ch', 'xuexiao', 'xuexiaoleibie', 'xueli_leibie', 'xueli_chengdu',
                             'gpa', 'ielts', 'tofel', 'gre', 'gmat']}),
