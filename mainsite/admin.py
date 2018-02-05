01f24daa9395dcb2163bb9a44193b6a661098c2a
liqiang <1936580@qq.com>
2018-02-05 16:00:21 +0800

    add guwen_name,anli_tese

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -6,7 +6,7 @@ class BenkeAdmin(admin.ModelAdmin):
     list_display = ('id', 'zhongwen', 'jigou', 'gpa', 'tofel', 'ielts', 'sat', 'act')
     fieldsets = [
         ('学校信息', {'fields':['liuxue_leixing', 'offer_huoqu', 'tiaojian', 'guojia', 'zhongwen', 'xueli', 'xuezhi', 'jiangxuejin', 'zhuanye', 'area']}),
-        ('代理信息', {'fields':['jigou', 'jigou_city', 'url']}),
+        ('代理信息', {'fields':['jigou', 'jigou_city', 'guwen_name', 'url', 'anli_tese' ]}),
         ('学员信息', {'fields':['chengshi', 'xuexiao', 'xuexiaoleibie', 'xueli_leibie', 'xueli_chengdu', 'gaokao',
                             'gpa', 'ielts', 'tofel', 'sat', 'act']}),
     ]
@@ -19,7 +19,7 @@ class ShuoshiAdmin(admin.ModelAdmin):
     list_display = ('id', 'zhongwen', 'jigou', 'gpa', 'tofel', 'ielts', 'gre', 'gmat')
     fieldsets = [
         ('学校信息', {'fields':['liuxue_leixing', 'offer_huoqu', 'tiaojian', 'guojia', 'zhongwen', 'xueli', 'xuezhi', 'jiangxuejin', 'zhuanye', 'area']}),
-        ('代理信息', {'fields':['jigou', 'jigou_city', 'url']}),
+        ('代理信息', {'fields':['jigou', 'jigou_city','guwen_name',  'url', 'anli_tese']}),
         ('学员信息', {'fields':['chengshi', 'zhuanye_ch', 'xuexiao', 'xuexiaoleibie', 'xueli_leibie', 'xueli_chengdu',
                             'gpa', 'ielts', 'tofel', 'gre', 'gmat']}),
     ]
